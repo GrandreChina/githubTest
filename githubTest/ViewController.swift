34335edf89790457f480e9840a10f1782c257f2b
GrandreChina <Grandre@zhangyingdemacbook-air.local>
2017-04-13 10:12:06 +0800

    Initial Commit

---
+++ githubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubTest
+//
+//  Created by Grandre on 17/4/13.
+//  Copyright © 2017年 革码者. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
